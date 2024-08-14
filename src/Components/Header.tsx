d28ec58f95f03eaa70ab24c09f620d378decd8a2
Felipe Manchester <f.p2809@gmail.com>
2024-08-14 09:44:24 -0300

    fix names

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from "react";
-import DateRange from "./DateRange";
-import Months from "./Months";
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import DateRange from './DateRange';
+import Months from './Months';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
-  const [title, setTitle] = useState("Resumo");
+  const [title, setTitle] = useState('Resumo');
   const location = useLocation();
 
   useEffect(() => {
-    if (location.pathname === "/") {
-      setTitle("Resumo");
-      document.title = "Fintech | Resumo";
-    } else if (location.pathname === "/sales") {
-      setTitle("Vendas");
-      document.title = "Fintech | Vendas";
+    if (location.pathname === '/') {
+      setTitle('Resumo');
+      document.title = 'Dashboard | Resumo';
+    } else if (location.pathname === '/sales') {
+      setTitle('Vendas');
+      document.title = 'Dashboard | Vendas';
     }
   }, [location]);
 
   return (
-    <header className="mb">
-      <div className="daterange mb">
+    <header className='mb'>
+      <div className='daterange mb'>
         <DateRange />
-        <h1 className="box bg-3">{title}</h1>
+        <h1 className='box bg-3'>{title}</h1>
       </div>
       <Months />
     </header>
